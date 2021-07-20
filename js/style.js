a37e17d5c4b0e6f6df08370dbf684f022143ff20
Sajedur Rahman <sajedurrahmanratul52@gmail.com>
2021-07-20 11:26:19 -0700

    last footer remain

--- js/style.js
+++ js/style.js
@@ -37,3 +37,43 @@ function stickyNav() {
 
 stickyNav()
 
+function hover() {
+    //first
+    const hover1 = document.querySelector(".img1");
+    const underline1 = document.querySelector(".underline1")
+
+    //first funtion
+    hover1.addEventListener("mouseenter", () => {
+        underline1.classList.add("h3")
+    })
+    hover1.addEventListener("mouseleave", () => {
+        underline1.classList.remove("h3")
+    })
+
+    //second
+    const hover2 = document.querySelector(".img2");
+    const underline2 = document.querySelector(".underline2")
+    //second funtion
+    hover2.addEventListener("mouseenter", () => {
+        underline2.classList.add("h3")
+    })
+    hover2.addEventListener("mouseleave", () => {
+        underline2.classList.remove("h3")
+    })
+
+    //third
+    const hover3 = document.querySelector(".img3");
+    const underline3 = document.querySelector(".underline3")
+    //first funtion
+    hover3.addEventListener("mouseenter", () => {
+        underline3.classList.add("h3")
+    })
+    hover3.addEventListener("mouseleave", () => {
+        underline3.classList.remove("h3")
+    })
+}
+
+
+hover()
+
+
